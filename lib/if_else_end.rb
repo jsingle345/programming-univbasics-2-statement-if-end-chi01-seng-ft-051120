9413d21af1ea297b38c1e6b93d0fea9c0098e109
Joshua Singleton <jsingle345@gmail.com>
2020-04-27 23:01:41 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
 # Write your solution here
 
-run_code_inside = true 
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside" 
-end 
-puts "Code after if...end"
+time = Times.now
+if time == even 
+  puts "Even!"
+else
+  puts "Odd!"
+end
